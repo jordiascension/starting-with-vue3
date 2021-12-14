3b19edaf78150874af348161d0fd26b74cd4920e
jordiascension <jordiascension@enterpriseconsulting.es>
2021-12-14 16:22:59 +0100

    add StartingWithVue example with message

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  <Starting message="Welcome to Your Vue.js App" type="C"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import Starting from './components/StartingWithVue.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Starting
   }
 }
 </script>
